7ca2a88870721ad96f04055914ea838c3e6f3b39
kirino <2867105717@qq.com>
2023-03-26 11:22:52 +0800

    主题逻辑的实现

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 import cv2
-from pyzbar import pyzbar
+import pyzbar.pyzbar as pbar
 from time import sleep
+import numpy as np
+import os
+import argparse
+import imutils
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
-interval = 1
+def main_part():
+    # Initialize the camera
+    cap = cv2.VideoCapture(0)
+    interval = 0.5
+    jk = 0
+
+    while True:
+        # Capture a frame from the camera
+        f, frame = cap.read()               # 将摄像头中的一帧图片数据保存
+        cv2.imwrite(f'D:/git/code/project/body/barcodeScaner/result{jk}.jpg', frame)     # 将图片保存为本地文件
+        frame = cv2.imread(f'D:/git/code/project/body/barcodeScaner/result{jk}.jpg')    #读取图片
+        gray = cv2.cvtColor(frame, 0)
+        # Decode the barcode from the frame 解码
+        barcodes = pbar.decode(gray)
+
+        if barcodes == []:
+            print("Error decoding barcodes")
+        else:
+            print("识别成功")
+            for con in barcodes:
+                barcodes_data = con.data.decode("utf-8")
+                print("识别结果是:" ,end="")
+                print(barcodes_data)
 
 
-while True:
-    # Capture a frame from the camera
-    # ret, frame = cap.read()
-    frame = cv2.imread('test.png')
+        #删除垃圾
+        os.remove(f"D:/git/code/project/body/barcodeScaner/result{jk}.jpg")
+        #index++
+        jk += 1
 
-    # Decode the barcode from the frame
-    barcodes = pyzbar.decode(frame)
+        #  Display the frame
+        cv2.imshow('Barcode Scanner', frame)
+        sleep(interval)
 
-    # Loop through all the barcodes found in the frame
-    for barcode in barcodes:
-        # Extract the barcode data and type
-        barcode_data = barcode.data.decode('utf-8')
-        barcode_type = barcode.type
+        # Check for key press
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
 
-        # Print the barcode data and type
-        print(f"Barcode data: {barcode_data}, Barcode type: {barcode_type}")
+    print(f"一共截下{jk}张照片,即检查产品{jk}个。")
+    # Release the camera and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
 
-    # Display the frame
-    cv2.imshow('Barcode Scanner', frame)
-
-    # Check for key press
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-    sleep(interval)
-
-# Release the camera and close all windows
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    # todo 修改为你要进行检测的图片路径即可
+    main_part()
